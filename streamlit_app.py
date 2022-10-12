72080028ed3bfbc790a2b524f421f20ba397b2e3
Venkatesh Rangarajan <115641485+vsrf18@users.noreply.github.com>
2022-10-12 14:21:06 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,6 @@ streamlit.text('🥣 Omega 3 and Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑 Avacado Toast')
+
+
+streamlit.header('🍌🥭Build your Own Fruit Smoothie🥝🍇')
