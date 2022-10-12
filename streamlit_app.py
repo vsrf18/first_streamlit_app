8468c34c36420d635528925747f6a31cc7f863d8
Venkatesh Rangarajan <115641485+vsrf18@users.noreply.github.com>
2022-10-12 14:43:20 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,3 +10,7 @@ streamlit.text('🥑 Avacado Toast')
 
 
 streamlit.header('🍌🥭Build your Own Fruit Smoothie🥝🍇')
+
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
